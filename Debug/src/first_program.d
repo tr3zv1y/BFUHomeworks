1f8bbb23d24ea2625a34f48aecbcd31e7b605dcc
tr3zv1y <alexey.veliky@gmail.com>
2023-09-16 09:15:48 +0200

    initialization commit

---
+++ Debug/src/first_program.d
@@ -0,0 +1,1 @@
+src/first_program.o: ../src/first_program.cpp
---
+++ src/first_program.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : firstProject.cpp
+// Author      : tr3zv1y
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	cout << "It was strange\n";
+
+	return 0;
+}
